fadd30aea4c90723678ee93fc99ed0b107a5eaa5
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-03-23 23:44:11 +0530

    practice

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,26 @@
-console.log(a)
+console.log(a);
 var a = 10;
 
-console.log(b)
+console.log(b);
 const b = 10;
-const b = 20;
+
+//use before declaration
+console.log(a + 5);
+var a = 10;
+
+if (a == undefined) {
+  console.log("a exist karta hai but value nahi hai");
+}
+
+var a = 20;
+
+//Example 2 (math me use karte hain)
+console.log(a + 5);
+let a = 10;
+
+//Example 3 (condition me use karte hain)
+if (a == undefined) {
+  console.log("hello");
+}
+
+let a = 20;
